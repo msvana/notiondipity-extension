6d29a7d68a715a26fdd84fa529f5ff88d096c041
Milos Svana <milos.svana@gmail.com>
2023-05-02 09:42:52 +0200

    Show error if not on a Notion page

--- src/Home.vue
+++ src/Home.vue
@@ -1,26 +1,28 @@
 <template>
-    <div v-if="loading">
-        Loading similar pages ...
-    </div>
+    <div v-if="loading">Loading similar pages ...</div>
     <div v-else>
-        <h1>
-            <small>Pages similar to</small><br /><span>{{ currentPageName }}</span>
-        </h1>
-        <ul>
-            <li v-for="recommendation in recommendedPagesList">
-                <a :href="recommendation[0]">{{ recommendation[0] }}</a>
-            </li>
-        </ul>
+        <div v-if="error">Looks like your current active tab is not a Notion page!</div>
+        <div v-else>
+            <h1>
+                <small>Pages similar to</small><br /><span>{{ currentPageName }}</span>
+            </h1>
+            <ul>
+                <li v-for="recommendation in recommendedPagesList">
+                    <a :href="recommendation[0]">{{ recommendation[0] }}</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from "vue";
-import type { CurrentPageReponse, Recommendation, RecommendationResponse } from "@/Models";
+import type { CurrentPageReponse, Recommendation, RecommendationResponse } from "./Models";
 
 const currentPageName = ref<string>("");
 const recommendedPagesList = ref<Recommendation[]>([]);
-const loading = ref<boolean>(true)
+const loading = ref<boolean>(true);
+const error = ref<boolean>(false);
 
 onMounted(async function () {
     browser.runtime.sendMessage({ type: "get-page-id" }).then((response: CurrentPageReponse) => {
@@ -30,25 +32,25 @@ onMounted(async function () {
 
 async function loadRecommendations(pageId: string | null) {
     if (pageId === null) {
-        return;
+        error.value = true;
+    } else {
+        const recommendationResponse = await getRecommendations(pageId);
+        currentPageName.value = recommendationResponse.currentPage.title;
+        recommendedPagesList.value = recommendationResponse.recommendations;
     }
 
-
-    const recommendationResponse = await getRecommendations(pageId);
-    currentPageName.value = recommendationResponse.currentPage.title;
-    recommendedPagesList.value = recommendationResponse.recommendations;
-    loading.value = false
+    loading.value = false;
 }
 
 async function getRecommendations(pageId: string): Promise<RecommendationResponse> {
     const url = `http://localhost:5001/recommend/${pageId}`;
-    const accessTokenResponse = await browser.runtime.sendMessage({type: 'get-access-token'})
-    const accessToken = accessTokenResponse['accessToken']
+    const accessTokenResponse = await browser.runtime.sendMessage({ type: "get-access-token" });
+    const accessToken = accessTokenResponse["accessToken"];
 
     const response = await fetch(url, {
         method: "GET",
         mode: "cors",
-        headers: {Authorization: `Bearer ${accessToken}`}
+        headers: { Authorization: `Bearer ${accessToken}` },
     });
 
     const result = await response.json();
